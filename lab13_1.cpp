a4088d92f3e7c1d8c6dff8a3bcc118bfb6ad5d51
EzPp01 <krittapat_karnna@cmu.ac.th>
2026-01-09 20:07:11 +0700

    Update lab13_1.cpp

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,26 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+void stat(const double A[], int N, double B[]){
+    double sum = 0.0;
+    double sumSq = 0.0;
+    double geo = 1.0;
+    double harm = 0.0;
+    double maxVal = A[0];
+    double minVal = A[0];
+    for (int i = 0; i < N; i++){
+        sum += A[i];
+        geo *= A[i];
+        harm += 1.0 / A[i];
+        sumSq += A[i] * A[i];
+
+        if (A[i] > maxVal) maxVal = A[i];
+        if (A[i] < minVal) minVal = A[i];
+    }
+    B[0] = sum / N;
+    B[1] = sqrt((sumSq / N) - (B[0] * B[0]));
+    B[2] = pow(geo, 1.0 / N);
+    B[3] = N / harm;
+    B[4] = maxVal;
+    B[5] = minVal;
+}
